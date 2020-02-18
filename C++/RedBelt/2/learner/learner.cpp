6cfb07da24f5598a475a9cda1a60dfd87ee570f2
Виктор Краснов <vickr@inbox.ru>
2020-02-18 12:14:46 +0300

    * C++/RedBelt/2/learner: done

--- C++/RedBelt/2/learner/learner.cpp
+++ C++/RedBelt/2/learner/learner.cpp
@@ -1,28 +1,27 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 class Learner {
  private:
-  vector<string> dict;
+  set<string> dict;
 
  public:
   int Learn(const vector<string>& words) {
     int newWords = 0;
     for (const auto& word : words) {
-      if (find(dict.begin(), dict.end(), word) == dict.end()) {
+      if (dict.find(word) == dict.end()) {
         ++newWords;
-        dict.push_back(word);
+        dict.insert(word);
       }
     }
     return newWords;
   }
 
   vector<string> KnownWords() {
-    sort(dict.begin(), dict.end());
-    dict.erase(unique(dict.begin(), dict.end()), dict.end());
-    return dict;
+    return { dict.begin(), dict.end() };
   }
 };

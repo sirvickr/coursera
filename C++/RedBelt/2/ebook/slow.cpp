4a052fb8fb57cad0845d567989f44a1da4a37278
Виктор Краснов <vickr@inbox.ru>
2020-02-21 09:00:13 +0300

    * C++/RedBelt/2/ebook: done

--- C++/RedBelt/2/ebook/slow.cpp
+++ C++/RedBelt/2/ebook/slow.cpp
@@ -1,4 +1,5 @@
 #include <iomanip>
+#include <sstream>
 #include <iostream>
 #include <vector>
 #include <utility>
@@ -12,18 +13,17 @@ class ReadingManager {
 public:
   ReadingManager()
       : user_page_counts_(MAX_USER_COUNT_ + 1, 0),
-        sorted_users_(),
-        user_positions_(MAX_USER_COUNT_ + 1, -1) {}
+        users_on_page_(MAX_PAGE_COUNT_ + 1, 0),
+        user_count_(0) {}
 
   void Read(int user_id, int page_count) {
     if (user_page_counts_[user_id] == 0) {
-      AddUser(user_id);
+      ++user_count_; // новый читатель
+    } else {
+      --users_on_page_[user_page_counts_[user_id]]; // пользователь ушёл с этой страницы
     }
+    ++users_on_page_[page_count];        // и остановился на этой
     user_page_counts_[user_id] = page_count;
-    int& position = user_positions_[user_id];
-    while (position > 0 && page_count > user_page_counts_[sorted_users_[position - 1]]) {
-      SwapUsers(position, position - 1);
-    }
   }
 
   double Cheer(int user_id) const {
@@ -35,18 +35,15 @@ public:
       return 1;
     }
     const int page_count = user_page_counts_[user_id];
-    int position = user_positions_[user_id];
-    while (position < user_count &&
-      user_page_counts_[sorted_users_[position]] == page_count) {
-      ++position;
+    int looser_count = 0; // счётчик числа пользователей, остановившихся на предыдущих страницах
+    for(int i = 1; i < page_count; ++i) {
+      looser_count += users_on_page_[i];
     }
-    if (position == user_count) {
-        return 0;
-    }
+
     // По умолчанию деление целочисленное, поэтому
     // нужно привести числитель к типу double.
     // Простой способ сделать это — умножить его на 1.0.
-    return (user_count - position) * 1.0 / (user_count - 1);
+    return looser_count * 1.0 / (user_count - 1);
   }
 
 private:
@@ -56,23 +53,14 @@ private:
   // Будь она публичной, к ней можно было бы обратиться снаружи
   // следующим образом: ReadingManager::MAX_USER_COUNT.
   static const int MAX_USER_COUNT_ = 100'000;
+  static const int MAX_PAGE_COUNT_ =   1'000;
 
   vector<int> user_page_counts_;
-  vector<int> sorted_users_;   // отсортированы по убыванию количества страниц
-  vector<int> user_positions_; // позиции в векторе sorted_users_
+  vector<int> users_on_page_; // количество пользователей в конце каждой страницы
+  int user_count_;
 
   int GetUserCount() const {
-    return sorted_users_.size();
-  }
-  void AddUser(int user_id) {
-    sorted_users_.push_back(user_id);
-    user_positions_[user_id] = sorted_users_.size() - 1;
-  }
-  void SwapUsers(int lhs_position, int rhs_position) {
-    const int lhs_id = sorted_users_[lhs_position];
-    const int rhs_id = sorted_users_[rhs_position];
-    swap(sorted_users_[lhs_position], sorted_users_[rhs_position]);
-    swap(user_positions_[lhs_id], user_positions_[rhs_id]);
+    return user_count_;
   }
 };
 

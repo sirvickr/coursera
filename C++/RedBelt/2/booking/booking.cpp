d733b458e9bf62f70ffed2b47149d70ac93a7aed
Виктор Краснов <vickr@inbox.ru>
2020-03-10 20:24:19 +0300

    * C++/RedBelt/2/booking draft

--- C++/RedBelt/2/booking/booking.cpp
+++ C++/RedBelt/2/booking/booking.cpp
@@ -2,12 +2,35 @@
 #include <sstream>
 #include <iostream>
 #include <vector>
+#include <set>
+#include <map>
 #include <utility>
 
 #include "test_runner.h"
 #include "profile.h"
 
 using namespace std;
+
+struct BookingItem {
+  //int book_time = 0;
+  int client_id = 0;
+  int room_count = 0;
+};
+
+ostream& operator <<(ostream& stream, const BookingItem& item) {
+  stream << "(" << /*item.book_time << ", " <<*/ item.client_id << ", " << item.room_count << ")";
+  return stream;
+}
+
+/*bool operator <(const BookingItem& lhs, const BookingItem& rhs) {
+  return lhs.book_time < rhs.book_time;
+}*/
+
+struct Hotel {
+  int clients = 0;
+  int booked_rooms = 0;
+  map<int, BookingItem> booking;
+};
 
 class BookingManager {
 public:
@@ -16,22 +39,45 @@ public:
   {}
 
   void Book(const string& hotel_name, int book_time, int client_id, int room_count) {
+    //cerr << "Book: hotel_name " << hotel_name << " book_time " << book_time << " client_id " << client_id << " room_count " << room_count << endl;
     current_time_ = book_time;
-    cerr << "Book: hotel_name " << hotel_name << " book_time " << book_time << " client_id " << client_id << " room_count " << room_count << endl;
+    auto hotel = hotels_.find(hotel_name);
+    if(hotel == hotels_.end()) {
+      hotel = hotels_.insert({hotel_name, {}}).first;
+      cerr << "Book: inserted hotel " << hotel->first << endl;
+    } else {
+      cerr << "Book: found hotel " << hotel->first << ": clients " << hotel->second.clients << " rooms " << hotel->second.booked_rooms << " bookings " << hotel->second.booking << endl;
+    }
+    // erase: log(c.size()) + std::distance(first, last)
+    //hotel->second.booking.insert({book_time, client_id, room_count});
+    hotel->second.booking.insert({book_time, {client_id, room_count}});
   }
 
   int Clients(const string& hotel_name) {
-    cerr << "Clients: hotel_name " << hotel_name << endl;
+    //cerr << "Clients: hotel_name " << hotel_name << endl;
+    auto hotel = hotels_.find(hotel_name);
+    if(hotel == hotels_.end()) {
+      return 0;
+    }
+    cerr << "Clients: found hotel " << hotel->first << ": clients " << hotel->second.clients << " rooms " << hotel->second.booked_rooms << " bookings " << hotel->second.booking << endl;
     return 0;
   }
 
   int Rooms(const string& hotel_name) {
-    cerr << "Rooms: hotel_name " << hotel_name << endl;
+    //cerr << "Rooms: hotel_name " << hotel_name << endl;
+    auto hotel = hotels_.find(hotel_name);
+    if(hotel == hotels_.end()) {
+      return 0;
+    }
+    cerr << "Rooms: found hotel " << hotel->first << ": clients " << hotel->second.clients << " rooms " << hotel->second.booked_rooms << " bookings " << hotel->second.booking << endl;
     return 0;
   }
 
 private:
+  static const int MAX_TIME_SPAN = 86400;
+
   int current_time_;
+  map<string, Hotel> hotels_;
 };
 
 void TestSample() {

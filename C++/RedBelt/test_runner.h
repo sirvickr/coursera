a3a421daef2c231b6579cb7ef4b747eb507b6c0b
Виктор Краснов <vickr@inbox.ru>
2020-08-28 12:30:20 +0300

    * C++/RedBelt/test_runner.h: removed output operators for collections - ones from ../collout.h will be used instead

--- C++/RedBelt/test_runner.h
+++ C++/RedBelt/test_runner.h
@@ -1,56 +1,13 @@
 #pragma once
 
+#include "../collout.h"
+
 #include <sstream>
 #include <stdexcept>
 #include <iostream>
-#include <map>
-#include <set>
 #include <string>
-#include <vector>
 
 using namespace std;
-
-template <class T>
-ostream& operator << (ostream& os, const vector<T>& s) {
-  os << "{";
-  bool first = true;
-  for (const auto& x : s) {
-    if (!first) {
-      os << ", ";
-    }
-    first = false;
-    os << x;
-  }
-  return os << "}";
-}
-
-template <class T>
-ostream& operator << (ostream& os, const set<T>& s) {
-  os << "{";
-  bool first = true;
-  for (const auto& x : s) {
-    if (!first) {
-      os << ", ";
-    }
-    first = false;
-    os << x;
-  }
-  return os << "}";
-}
-
-template <class K, class V>
-ostream& operator << (ostream& os, const map<K, V>& m) {
-  os << "{";
-  bool first = true;
-  for (const auto& kv : m) {
-    if (!first) {
-      os << ", ";
-    }
-    first = false;
-    os << kv.first << ": " << kv.second;
-  }
-  return os << "}";
-}
 
 template<class T, class U>
 void AssertEqual(const T& t, const U& u, const string& hint = {}) {

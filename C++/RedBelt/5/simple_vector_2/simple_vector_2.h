cc1e8b0da2259bb4acfda23ac44c481610dfcc50
Виктор Краснов <vickr@inbox.ru>
2020-09-14 16:05:05 +0300

    * C++/RedBelt/5/simple_vector_2 (Move-семантика в SimpleVector): заготовка из предыдущего задания

--- C++/RedBelt/5/simple_vector_2/simple_vector_2.h
+++ C++/RedBelt/5/simple_vector_2/simple_vector_2.h
@@ -1,29 +1,109 @@
-#include <cstdint>
+#pragma once
 
-using namespace std;
-
-// Реализуйте SimpleVector в этом файле
-// и отправьте его на проверку
+#include <cstdlib>
+#include <algorithm>
 
 template <typename T>
 class SimpleVector {
 public:
   SimpleVector() = default;
   explicit SimpleVector(size_t size);
+  SimpleVector(const SimpleVector& other);
+  SimpleVector& operator=(const SimpleVector& other);
   ~SimpleVector();
 
   T& operator[](size_t index);
 
-  T* begin();
-  T* end();
+  T* begin() {
+    return begin_;
+  }
+  T* end() {
+    return end_;
+  }
 
-  size_t Size() const;
-  size_t Capacity() const;
+  const T* begin() const {
+    return begin_;
+  }
+  const T* end() const {
+    return end_;
+  }
+
+  size_t Size() const {
+    return size_;
+  }
+  size_t Capacity() const {
+    return capacity_;
+  }
   void PushBack(const T& value);
 
-  // При необходимости перегрузите
-  // существующие публичные методы
+private:
+  void Allocate(size_t size, size_t capacity);
 
 private:
-  // Добавьте сюда поля
+  size_t size_ = 0;
+  size_t capacity_ = 0;
+  T* begin_ = nullptr;
+  T* end_ = nullptr;
 };
+
+template <typename T>
+SimpleVector<T>::SimpleVector(size_t size) {
+  Allocate(size, size);
+}
+
+template <typename T>
+SimpleVector<T>::SimpleVector(const SimpleVector& other) {
+  Allocate(other.size_, other.capacity_);
+  std::copy(begin(other), end(other), begin_);
+}
+
+template <typename T>
+SimpleVector<T>& SimpleVector<T>::operator=(const SimpleVector& other) {
+  Allocate(other.size_, other.capacity_);
+  std::copy(other.begin_, other.end_, begin_);
+  return *this;
+}
+
+template <typename T>
+SimpleVector<T>::~SimpleVector() {
+  if(begin_) {
+    delete[] begin_;
+  }
+}
+
+template <typename T>
+T& SimpleVector<T>::operator[](size_t index) {
+  return begin_[index];
+}
+
+template <typename T>
+void SimpleVector<T>::PushBack(const T& value) {
+  if(size_ == capacity_) {
+    if(capacity_ > 0)
+      capacity_ <<= 1;
+    else
+      capacity_ = 1;
+    T* data = new T[capacity_];
+    if(begin_) {
+      std::copy(begin_, end_, data);
+      delete[] begin_;
+    }
+    begin_ = data;
+  }
+  begin_[size_++] = value;
+  end_ = begin_ + size_;
+}
+
+template <typename T>
+void SimpleVector<T>::Allocate(size_t size, size_t capacity) {
+  if(begin_) {
+    delete [] begin_;
+    begin_ = end_ = nullptr;
+  }
+  size_= size;
+  capacity_ = capacity;
+  if(capacity_) {
+    begin_ = new T[capacity_];
+    end_ = begin_ + size_;
+  }
+}

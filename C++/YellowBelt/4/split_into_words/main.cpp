0e8998d5c89ced69b8fa521d3654fce66f25608c
Виктор Краснов <vickr@inbox.ru>
2020-09-30 13:40:57 +0300

    * C++/YellowBelt/4/split_into_words (Разбиение на слова): done

--- C++/YellowBelt/4/split_into_words/main.cpp
+++ C++/YellowBelt/4/split_into_words/main.cpp
@@ -1,14 +1,39 @@
+#include "test_runner.h"
+
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 vector<string> SplitIntoWords(const string& s) {
-  return {};
+  vector<string> result;
+  auto from = begin(s);
+  auto to = find(from, end(s), ' ');
+  while(to != end(s)) {
+    result.push_back(string(from, to));
+    from = next(to);
+    to = find(from, end(s), ' ');
+  }
+  if(from != to) {
+    result.push_back(string(from, to));
+  }
+  return result;
+}
+
+void TestAll() {
+  AssertEqual(SplitIntoWords(""), vector<string>{}, "1");
+  AssertEqual(SplitIntoWords("C"), vector<string>{"C"}, "2");
+  AssertEqual(SplitIntoWords("C"), vector<string>{"C"}, "3");
+  AssertEqual(SplitIntoWords("C "), vector<string>{"C"}, "4");
+  AssertEqual(SplitIntoWords("C Cpp Java Python"), vector<string>{"C", "Cpp", "Java", "Python"}, "5");
 }
 
 int main() {
+  TestRunner tr;
+  tr.RunTest(TestAll, "TestAll");
+
   string s = "C Cpp Java Python";
 
   vector<string> words = SplitIntoWords(s);

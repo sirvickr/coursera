1caf71d87c400ebf7b2e34e0d9ab3e2de61f031b
Виктор Краснов <vickr@inbox.ru>
2020-09-30 11:23:52 +0300

    * C++/YellowBelt/4/set_part (Часть множества): done

--- C++/YellowBelt/4/set_part/main.cpp
+++ C++/YellowBelt/4/set_part/main.cpp
@@ -1,3 +1,5 @@
+#include "test_runner.h"
+
 #include <iostream>
 #include <set>
 #include <vector>
@@ -7,9 +9,22 @@ using namespace std;
 // Возвращает вектор из всех элементов множества, бо́льших border, в возрастающем порядке.
 template <typename T>
 vector<T> FindGreaterElements(const set<T>& elements, const T& border) {
+  auto it = elements.find(border);
+  if(it != end(elements))
+    ++it;
+  return vector<T>(it, end(elements));
+}
+
+void TestAll() {
+  AssertEqual(FindGreaterElements(set<int>{1, 5, 7, 8}, 5), vector<int>{7, 8}, "test-1");
+  AssertEqual(FindGreaterElements(set<string>{"C", "C++"}, string("Python")).size(), 0u, "test-2");
+  AssertEqual(FindGreaterElements(set<int>{1, 5, 7, 8}, -1), vector<int>{}, "test-1");
 }
 
 int main() {
+  TestRunner tr;
+  tr.RunTest(TestAll, "TestAll");
+
   for (int x : FindGreaterElements(set<int>{1, 5, 7, 8}, 5)) {
     cout << x << " ";
   }

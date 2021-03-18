ff5ff0ebc4181400cf7f25cfc3ad527b41ad5d20
Виктор Краснов <vickr@inbox.ru>
2021-03-18 14:24:22 +0300

    * * C++/YellowBelt/1/sqr (Всё в квадрат): solution

--- C++/YellowBelt/1/sqr/sqr.cpp
+++ C++/YellowBelt/1/sqr/sqr.cpp
@@ -1,66 +1,50 @@
-#include <iostream>
-#include <vector>
-#include <map>
+// Объявим функции без тела,
+// чтобы их можно было использовать в произвольном порядке
 
-using namespace std;
+// Для простых типов
+template<typename T>
+T Sqr(T value);
 
-template<typename Seq> Seq SeqSqr(const Seq& seq);
-template<typename T> T Sqr(const T& x);
-template<typename K, typename V> pair<K, V> Sqr(const pair<K, V>& x);
-template<typename T> vector<T> Sqr(const vector<T>& x);
-template<typename K, typename V> map<K, V> Sqr(const map<K, V>& x);
+// Для вектора
+template<typename T>
+vector<T> Sqr(const vector<T>& value);
 
-template<typename Seq>
-Seq SeqSqr(const Seq& seq) {
-	Seq result = seq;
-	for(auto& item: result) {
-		item = Sqr(item);
-	}
-	return result;
+// Для словаря
+template<typename K, typename V>
+map<K, V> Sqr(const map<K, V>& value);
+
+// Для пары
+template<typename K, typename V>
+pair<K, V> Sqr(const pair<K, V>& value);
+
+// Теперь реализуем их в соответствии с заданием
+template<typename T>
+T Sqr(T value) {
+  return value * value;
 }
 
 template<typename T>
-T Sqr(const T& x) {
-	return x * x;
+vector<T> Sqr(const vector<T>& value) {
+  vector<T> result;
+  for (const T& x : value) {
+    // Здесь важно, что вместо x * x вызывается функция Sqr:
+    // это позволяет возводить в квадрат составные объекты
+    result.push_back(Sqr(x));
+  }
+
+  return result;
 }
 
 template<typename K, typename V>
-pair<K, V> Sqr(const pair<K, V>& x) {
-	return {Sqr(x.first), Sqr(x.second)};
-}
-
-template <typename T>
-vector<T> Sqr(const vector<T>& x) {
-	return SeqSqr(x);
+map<K, V> Sqr(const map<K, V>& value) {
+  map<K, V> result;
+  for (const auto& element : value) {
+    result[element.first] = Sqr(element.second);
+  }
+  return result;
 }
 
 template<typename K, typename V>
-map<K, V> Sqr(const map<K, V>& x) {
-	map<K, V> result = x;
-	for(auto& item: result) {
-		item.second = Sqr(item.second);
-	}
-	return result;
+pair<K, V> Sqr(const pair<K, V>& value) {
+  return {Sqr(value.first), Sqr(value.second)};
 }
-
-#if 1 // 0 для отправки
-int main() {
-	vector<int> v = {1, 2, 3};
-	cout << "vector:";
-	for (int x : Sqr(v)) {
-	  cout << ' ' << x;
-	}
-	cout << endl;
-
-	map<int, pair<int, int>> map_of_pairs = {
-	  {4, {2, 2}},
-	  {7, {4, 3}}
-	};
-	cout << "map of pairs:" << endl;
-	for (const auto& x : Sqr(map_of_pairs)) {
-	  cout << x.first << ' ' << x.second.first << ' ' << x.second.second << endl;
-	}
-
-  return 0;
-}
-#endif

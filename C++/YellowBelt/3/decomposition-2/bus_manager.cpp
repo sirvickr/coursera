e7e53d3ac61e5ce60aecb2c2d7d155b9e5a7322d
Виктор Краснов <vickr@inbox.ru>
2021-03-24 10:00:51 +0300

    + C++/YellowBelt/3/decomposition-2 (Декомпозиция программы - 2): solution

--- C++/YellowBelt/3/decomposition-2/bus_manager.cpp
+++ C++/YellowBelt/3/decomposition-2/bus_manager.cpp
@@ -1,25 +1,34 @@
 #include "bus_manager.h"
 
-void BusManager::AddBus(const string& bus, const vector<string>& stops) {
+void BusManager::AddBus(
+    const string& bus,
+    const vector<string>& stops)
+{
   buses_to_stops.insert(make_pair(bus, stops));
   for (const auto& stop : stops) {
     stops_to_buses[stop].push_back(bus);
   }
 }
 
-BusesForStopResponse BusManager::GetBusesForStop(const string& stop) const {
-  if (stops_to_buses.count(stop) == 0) {
+BusesForStopResponse BusManager::GetBusesForStop(
+    const string& stop) const
+{
+  auto iter = stops_to_buses.find(stop);
+  if (iter == stops_to_buses.end()) {
     return BusesForStopResponse{vector<string>()};
   } else {
-    return BusesForStopResponse{stops_to_buses.at(stop)};
+    return BusesForStopResponse{iter->second};
   }
 }
 
-StopsForBusResponse BusManager::GetStopsForBus(const string& bus) const {
+StopsForBusResponse BusManager::GetStopsForBus(
+    const string& bus) const
+{
   vector<pair<string, vector<string>>> result;
 
-  if (buses_to_stops.count(bus) > 0) {
-    for (const auto& stop : buses_to_stops.at(bus)) {
+  auto iter = buses_to_stops.find(bus);
+  if (iter != buses_to_stops.end()) {
+    for (const auto& stop : iter->second) {
       result.push_back(make_pair(stop, stops_to_buses.at(stop)));
     }
   }

7e89e3c69e276bf26247fb988b0f5c75f2929fce
Виктор Краснов <vickr@inbox.ru>
2021-03-26 15:01:56 +0300

    * C++/YellowBelt/4/merge_sort (Сортировка слиянием): refactored

--- C++/YellowBelt/4/merge_sort/merge_sort_2.cpp
+++ C++/YellowBelt/4/merge_sort/merge_sort_2.cpp
@@ -11,15 +11,14 @@ void MergeSort(RandomIt range_begin, RandomIt range_end) {
 	if(size < 2)
 		return;
 	// 2. Создать вектор, содержащий все элементы текущего диапазона.
+	vector<typename RandomIt::value_type> elements(range_begin, range_end);
 	// 3. Разбить вектор на две равные части (в этой задаче гарантируется, что длина передаваемого диапазона является степенью двойки, так что вектор всегда можно разбить на две равные части).
-	auto middle = next(range_begin, size / 2);
-	vector<typename RandomIt::value_type> a(range_begin, middle);
-	vector<typename RandomIt::value_type> b(middle, range_end);
+  auto middle = next(begin(elements), size / 2);
 	// 4. Вызвать функцию MergeSort от каждой половины вектора.
-	MergeSort(begin(a), end(a));
-	MergeSort(begin(b), end(b));
+  MergeSort(begin(elements), middle);
+  MergeSort(middle, end(elements));
 	// 5. С помощью алгоритма std::merge слить отсортированные половины, записав полученный отсортированный диапазон вместо исходного.
-	merge(begin(a), end(a), begin(b), end(b), range_begin);
+  merge(begin(elements), middle, middle, end(elements), range_begin);
 }
 
 #if 0
--- C++/YellowBelt/4/merge_sort/merge_sort_3.cpp
+++ C++/YellowBelt/4/merge_sort/merge_sort_3.cpp
@@ -11,23 +11,21 @@ void MergeSort(RandomIt range_begin, RandomIt range_end) {
 	if(size < 2)
 		return;
 	// 2. Создать вектор, содержащий все элементы текущего диапазона.
+	vector<typename RandomIt::value_type> elements(range_begin, range_end);
 	// 3. Разбить вектор на 3 равные части (в этой задаче гарантируется, что длина передаваемого диапазона является степенью тройки, так что вектор всегда можно разбить на три равные части).
 	size_t segment_size = size / 3;
-	auto delim1 = next(range_begin, segment_size);
+	auto delim1 = next(begin(elements), segment_size);
 	auto delim2 = next(delim1, segment_size);
-	vector<typename RandomIt::value_type> a(range_begin, delim1);
-	vector<typename RandomIt::value_type> b(delim1, delim2);
-	vector<typename RandomIt::value_type> c(delim2, range_end);
 	// 4. Вызвать функцию MergeSort от каждой части вектора.
-	MergeSort(begin(a), end(a));
-	MergeSort(begin(b), end(b));
-	MergeSort(begin(c), end(c));
+	MergeSort(begin(elements), delim1);
+	MergeSort(delim1, delim2);
+	MergeSort(delim2, end(elements));
 	// 5. Слить первые две трети вектора с помощью алгоритма merge, сохранив результат во временный вектор с помощью back_inserter.
 	vector<typename RandomIt::value_type> temp;
 	temp.reserve(segment_size * 2);
-	merge(begin(a), end(a), begin(b), end(b), back_inserter(temp));
+	merge(begin(elements), delim1, delim1, delim2, back_inserter(temp));
 	// 6. Слить временный вектор из предыдущего пункта с последней третью вектора из п. 2, записав полученный отсортированный диапазон вместо исходного.
-	merge(begin(temp), end(temp), begin(c), end(c), range_begin);
+	merge(begin(temp), end(temp), delim2, end(elements), range_begin);
 }
 
 #if 0

f065291b3b553e3678f807e483efb62ad8d78ac2
Виктор Краснов <vickr@inbox.ru>
2021-03-26 11:22:26 +0300

    * C++/YellowBelt/4/permutations (Перебор перестановок): solution (just comments)

--- C++/YellowBelt/4/permutations/main.cpp
+++ C++/YellowBelt/4/permutations/main.cpp
@@ -9,11 +9,16 @@ int main() {
   size_t n;
   cin >> n;
   vector<size_t> v(n);
+  // http://ru.cppreference.com/w/cpp/algorithm/iota
+  // Заполняет диапазон последовательно возрастающими значениями
   iota(rbegin(v), rend(v), 1);
   do {
 	  for(int i : v)
 	    cout << i << ' ';
 	  cout << endl;
+	  // http://ru.cppreference.com/w/cpp/algorithm/prev_permutation
+	  // Преобразует диапазон в предыдущую (лексикографически) перестановку, если она существует, и возвращает true,
+	  // иначе (если не существует) - в последнюю (наибольшую) и возвращает false
   } while(prev_permutation(begin(v), end(v)));
 
   return 0;

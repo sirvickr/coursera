8004c8ca8d0a5e30516d2d30bc7d5e211ba45f4d
Виктор Краснов <vickr@inbox.ru>
2021-04-03 09:31:59 +0300

    * Pyton/1.Diving/1/sum_args (Сумма цифр в строке): solution

--- Python/1. Diving/1/sum_args/solution.py
+++ Python/1. Diving/1/sum_args/solution.py
@@ -1,9 +1,3 @@
 import sys
 
-if __name__ == "__main__":
-	digit_string = sys.argv[1]
-	sum = 0
-	for char in digit_string:
-		sum += int(char)
-
-	print(sum)
+print(sum([int(x) for x in sys.argv[1]]))

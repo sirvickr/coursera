740cb98f04ac24b50990960dcd47142e8a6529d3
Виктор Краснов <vickr@inbox.ru>
2021-04-05 22:34:56 +0300

    * Pyton/1.Diving/1/eqation (Корни квадратного уравнения): solution

--- Python/1. Diving/1/equation/solution.py
+++ Python/1. Diving/1/equation/solution.py
@@ -1,4 +1,5 @@
 import sys 
+import math
 
 """
 Корни квадратного уравнения
@@ -11,8 +12,8 @@ c = int(sys.argv[3])
 
 d = b * b - 4 * a * c
 
-x1 = (-b + d**0.5) / (2 * a)
-x2 = (-b - d**0.5) / (2 * a)
+x1 = (-b + math.sqrt(d)) / (2 * a)
+x2 = (-b - math.sqrt(d)) / (2 * a)
 
 print(int(x1))
 print(int(x2))

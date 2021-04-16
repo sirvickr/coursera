5b373ddd29548adae4a47e45bd97afd9b04e9f7f
Виктор Краснов <vickr@inbox.ru>
2021-04-16 12:39:07 +0300

    * C++/YellowBelt/6/kumite (Курсовой проект): final (?) code cleanup

--- C++/YellowBelt/6/kumite/database.h
+++ C++/YellowBelt/6/kumite/database.h
@@ -7,8 +7,7 @@
 #include <string>
 #include <list>
 #include <vector>
-#include <iostream> // TODO
-#include <algorithm>
+#include <ostream>
 
 using namespace std;
 
@@ -28,30 +27,21 @@ public:
   template<class Predicate>
   int RemoveIf(Predicate predicate) {
     int count = 0;
-    //cout << "RemoveIf" << endl;
     for(auto db_it = begin(storage); db_it != end(storage); ) {
-      //cout << " " << db_it->first << ":";
       for(auto it = begin(db_it->second.events); it != end(db_it->second.events); ) {
-        //cout << " {" << *it;
         if(predicate(db_it->first, *it)) {
-          //cout << " +";
           db_it->second.event_set.erase(*it);
           it = db_it->second.events.erase(it);
           ++count;
         } else {
-          //cout << " -";
          ++it;
         }
-        //cout << " }: ";
       }
-      //cout << db_it->second.event_set.size() << " entries overall";
       if(db_it->second.event_set.empty()) {
-        //cout << " (entry erased)";
         db_it = storage.erase(db_it);
       } else {
         ++db_it;
       }
-      //cout << endl;
     }
     return count;
   }
@@ -59,18 +49,12 @@ public:
   template<class Predicate>
   vector<pair<Date, string>> FindIf(Predicate predicate) const {
     vector<pair<Date, string>> result;
-    //cout << "FindIf" << endl;
     for(auto db_it = begin(storage); db_it != end(storage); ++db_it) {
-      //cout << " " << db_it->first << ":";
       for(auto it = begin(db_it->second.events); it != end(db_it->second.events); ++it) {
-        //cout << " {" << *it;
         if(predicate(db_it->first, *it)) {
-          //cout << " +";
           result.push_back({db_it->first, *it});
         }
-        //cout << " }: ";
       }
-      //cout << endl;
     }
     return result;
   }
--- C++/YellowBelt/6/kumite/main.cpp
+++ C++/YellowBelt/6/kumite/main.cpp
@@ -66,51 +66,6 @@ void Task(istream& is, ostream& os) {
 int main() {
   TestAll();
   Task(cin, cout);
-
-//  Database db;
-//
-//  for (string line; getline(cin, line); ) {
-//    istringstream is(line);
-//
-//    string command;
-//    is >> command;
-//    if (command == "Add") {
-//      const auto date = ParseDate(is);
-//      const auto event = ParseEvent(is);
-//      db.Add(date, event);
-//    } else if (command == "Print") {
-//      db.Print(cout);
-//    } else if (command == "Del") {
-//      auto condition = ParseCondition(is);
-//      auto predicate = [condition](const Date& date, const string& event) {
-//        return condition->Evaluate(date, event);
-//      };
-//      int count = db.RemoveIf(predicate);
-//      cout << "Removed " << count << " entries" << endl;
-//    } else if (command == "Find") {
-//      auto condition = ParseCondition(is);
-//      auto predicate = [condition](const Date& date, const string& event) {
-//        return condition->Evaluate(date, event);
-//      };
-//
-//      const auto entries = db.FindIf(predicate);
-//      for (const auto& entry : entries) {
-//        cout << entry << endl;
-//      }
-//      cout << "Found " << entries.size() << " entries" << endl;
-//    } else if (command == "Last") {
-//      try {
-//          cout << db.Last(ParseDate(is)) << endl;
-//      } catch (invalid_argument&) {
-//          cout << "No entries" << endl;
-//      }
-//    } else if (command.empty()) {
-//      continue;
-//    } else {
-//      throw logic_error("Unknown command: " + command);
-//    }
-//  }
-
   return 0;
 }
 
@@ -230,21 +185,6 @@ void TestTask() {
         "No entries\n");
     AssertEqual(result.str(), expected.str(), "Case 5");
   }
-  /*{
-    istringstream is(
-        "\n"
-        "\n"
-        "\n"
-        "\n"
-        "\n");
-    ostringstream result;
-    Task(is, result);
-    ostringstream expected(
-        "\n"
-        "\n"
-        "\n");
-    AssertEqual(result.str(), expected.str(), "Case 1");
-  }*/
 }
 
 void TestAll() {

92f4bc7cfa68770a9159cc2928370491a72794e2
Виктор Краснов <vickr@inbox.ru>
2021-05-03 14:19:07 +0300

    * Pyton/2.OOP/1/factorize (Создание модульного теста функции factorize): solution

--- Python/2. OOP/1. Structured Programming/factorize.py
+++ Python/2. OOP/1. Structured Programming/factorize.py
@@ -1,32 +1,48 @@
 
 class TestFactorize(unittest.TestCase):
-
     def test_wrong_types_raise_exception(self):
-        for x in ('string', 1.5):
+        """ аргументы типа float или str вызывают исключение TypeError """
+        for x in 1.5, 'string':
             with self.subTest(x=x):
-                self.assertRaises(TypeError, factorize, x)
+                with self.assertRaises(TypeError):
+                    factorize(x)
 
     def test_negative(self):
-        for x in (-1, -10, -100):
+        """ отрицательные числа вызывают исключение ValueError """
+        for x in -1, -10, -100:
             with self.subTest(x=x):
-                self.assertRaises(ValueError, factorize, x)
- 
+                with self.assertRaises(ValueError):
+                    factorize(x)
+
     def test_zero_and_one_cases(self):
-        for x in (0, 1):
+        """ целые чисела 0 и 1, возвращают кортежи (0,) и (1,) соответственно. """
+        for x in 0, 1:
             with self.subTest(x=x):
-                self.assertEqual(factorize(x=x), (x,))
- 
+                self.assertEqual(factorize(x), (x,))
+
     def test_simple_numbers(self):
-        for x in (3, 13, 29):
+        """ для простых чисел возвращается кортеж, содержащий одно данное число """
+        for x in 3, 13, 29:
             with self.subTest(x=x):
-                self.assertEqual(factorize(x=x), (x,))
+                self.assertEqual(factorize(x), (x,))
 
     def test_two_simple_multipliers(self):
-        for x, expected in {6: (2, 3), 26: (2, 13), 121: (11, 11)}.items():
+        """ числа для которых функция factorize возвращает кортеж размером 2 """
+        test_cases = (
+            (6, (2, 3)),
+            (26, (2, 13)),
+            (121, (11, 11)),
+        )
+        for x, expected in test_cases:
             with self.subTest(x=x):
-                self.assertEqual(factorize(x=x), expected)
+                self.assertEqual(factorize(x), expected)
 
     def test_many_multipliers(self):
-        for x, expected in {1001: (7, 11, 13), 9699690: (2, 3, 5, 7, 11, 13, 17, 19)}.items():
+        """ числа для которых функция factorize возвращает кортеж размером >2 """
+        test_cases = (
+            (1001, (7, 11, 13)),
+            (9699690, (2, 3, 5, 7, 11, 13, 17, 19)),
+        )
+        for x, expected in test_cases:
             with self.subTest(x=x):
-                self.assertEqual(factorize(x=x), expected)
+                self.assertEqual(factorize(x), expected)

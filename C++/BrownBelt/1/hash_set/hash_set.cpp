6da2d4fbe3cb87b4a2641bcfe4e0588be5a25be8
Виктор Краснов <vickr@inbox.ru>
2021-08-13 13:43:33 +0300

    * C++/BrownBelt/1/hash_set (Хеш-таблица): done = solution

--- C++/BrownBelt/1/hash_set/hash_set.cpp
+++ C++/BrownBelt/1/hash_set/hash_set.cpp
@@ -1,11 +1,13 @@
 #include "test_runner.h"
 
+#include <algorithm>
 #include <forward_list>
 #include <iterator>
+#include <vector>
 
 using namespace std;
 
-template <typename Type, typename Hasher>
+template <typename Type, typename Hasher = hash<Type>>
 class HashSet {
 public:
   using BucketList = forward_list<Type>;
@@ -14,12 +16,40 @@ public:
   explicit HashSet(
       size_t num_buckets,
       const Hasher& hasher = {}
-  );
+  )
+    : hasher_(hasher)
+    , buckets_(num_buckets)
+  {}
 
-  void Add(const Type& value);
-  bool Has(const Type& value) const;
-  void Erase(const Type& value);
-  const BucketList& GetBucket(const Type& value) const;
+  void Add(const Type& value) {
+    auto& bucket = buckets_[GetBucketIndex(value)];
+
+    if (!Has(value)) {
+      bucket.push_front(value);
+    }
+  }
+
+  bool Has(const Type& value) const {
+    const auto& bucket = GetBucket(value);
+    return find(begin(bucket), end(bucket), value) != bucket.end();
+  }
+
+  void Erase(const Type& value) {
+    buckets_[GetBucketIndex(value)].remove(value);
+  }
+
+  const BucketList& GetBucket(const Type& value) const {
+    return buckets_[GetBucketIndex(value)];
+  }
+
+private:
+  size_t GetBucketIndex(const Type& value) const {
+    return hasher_(value) % buckets_.size();
+  }
+    
+private:
+  Hasher hasher_;
+  vector<BucketList> buckets_;
 };
 
 struct IntHasher {

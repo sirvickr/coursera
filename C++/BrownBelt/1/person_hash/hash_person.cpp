a1e01fbc6d7a5a6233b23b7752568a5bbaa888bc
Виктор Краснов <vickr@inbox.ru>
2021-08-18 18:44:10 +0300

    * C++/BrownBelt/1/person_hash (Интеграция структуры Person в unordered_set): solution

--- C++/BrownBelt/1/person_hash/hash_person.cpp
+++ C++/BrownBelt/1/person_hash/hash_person.cpp
@@ -27,22 +27,36 @@ struct Person {
 
 struct AddressHasher {
   size_t operator() (const Address& address) const {
-      const size_t x = 2'946'901;
+      // выбираем в качестве коэффициента довольно большое простое число;
+      // свободный член можем положить равным нулю, т.к. он не влияет
+      // на коллизии, а лишь циклически смещает бакеты
+      const size_t x = 514'229;
+      // std::hash обычно не имеет состояния, поэтому создание
+      // локальных переменных не сопряжено с накладными расходами,
+      // а наоборот, помогает компилятору лучше оптимизировать код
+      hash<string> str_hasher;
+      hash<int> int_hasher;
+      
       return str_hasher(address.city) * x * x + str_hasher(address.street) * x + int_hasher(address.building);
   }
-  hash<string> str_hasher;
-  hash<int> int_hasher;
 };
 
 struct PersonHasher {
   size_t operator() (const Person& person) const {
-      const size_t x = 2'946'901;
+      // выбираем в качестве коэффициента довольно большое простое число;
+      // свободный член можем положить равным нулю, т.к. он не влияет
+      // на коллизии, а лишь циклически смещает бакеты
+      const size_t x = 39'916'801;
+      // std::hash обычно не имеет состояния, поэтому создание
+      // локальных переменных не сопряжено с накладными расходами,
+      // а наоборот, помогает компилятору лучше оптимизировать код
+      hash<string> str_hasher;
+      hash<int> int_hasher;
+      hash<double> dbl_hasher;
+      AddressHasher addr_hasher;
+      
       return str_hasher(person.name) * x * x * x + int_hasher(person.height) * x * x + dbl_hasher(person.weight) * x + addr_hasher(person.address);
   }
-  hash<string> str_hasher;
-  hash<int> int_hasher;
-  hash<double> dbl_hasher;
-  AddressHasher addr_hasher;
 };
 
 // сгенерированы командой:

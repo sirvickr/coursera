9c07a49af8206f7a1383b4ce352ba58f3b5f4342
Виктор Краснов <vickr@inbox.ru>
2021-08-26 23:02:36 +0300

    * C++/BrownBelt/2/spendings/ini (Библиотека работы с INI-файлами): solution

--- C++/BrownBelt/2/spendings/ini/ini.cpp
+++ C++/BrownBelt/2/spendings/ini/ini.cpp
@@ -1,44 +1,35 @@
 #include "ini.h"
+
 namespace Ini {
 
-pair<string_view, string_view> Split(string_view line, char by) {
-  size_t pos = line.find(by);
-  string_view left = line.substr(0, pos);
+Document Load(istream& input) {
+  Document result;
 
-  if (pos < line.size() && pos + 1 < line.size()) {
-    return {left, line.substr(pos + 1)};
-  } else {
-    return {left, string_view()};
+  Section* current_section = nullptr;
+  for (string line; getline(input, line); ) {
+    if (!line.empty()) {
+      if (line[0] == '[') {
+        current_section = &result.AddSection(line.substr(1, line.size() - 2));
+      } else {
+        size_t eq_pos = line.find('=');
+        current_section->insert({line.substr(0, eq_pos), line.substr(eq_pos + 1)});
+      }
+    }
   }
+
+  return result;
 }
 
 Section& Document::AddSection(string name) {
-    return sections[name];
+  return sections[name];
 }
 
 const Section& Document::GetSection(const string& name) const {
-    return sections.at(name);
+  return sections.at(name);
 }
 
 size_t Document::SectionCount() const {
-    return sections.size();
+  return sections.size();
 }
 
-Document Load(istream& input) {
-    Document doc;
-    string line;
-    Section* section = nullptr;
-    while(getline(input, line)) {
-        if(!line.empty()) {
-            if(line[0] == '[') {
-                section = &doc.AddSection(line.substr(1, line.size() - 2));
-            } else {
-                auto [name, value] = Split(line, '=');
-                (*section)[string(name)] = string(value);
-            }
-        }
-    }
-    return doc;
-}
-
-}
+} /* namespace Ini */

ffa7d19811bb78089100c5e29d14805f8796dc15
Виктор Краснов <vickr@inbox.ru>
2021-08-26 22:55:59 +0300

    * C++/BrownBelt/2/spendings/ini (Библиотека работы с INI-файлами): done

--- C++/BrownBelt/2/spendings/ini/ini.cpp
+++ C++/BrownBelt/2/spendings/ini/ini.cpp
@@ -1,6 +1,16 @@
 #include "ini.h"
-
 namespace Ini {
+
+pair<string_view, string_view> Split(string_view line, char by) {
+  size_t pos = line.find(by);
+  string_view left = line.substr(0, pos);
+
+  if (pos < line.size() && pos + 1 < line.size()) {
+    return {left, line.substr(pos + 1)};
+  } else {
+    return {left, string_view()};
+  }
+}
 
 Section& Document::AddSection(string name) {
     return sections[name];
@@ -11,7 +21,24 @@ const Section& Document::GetSection(const string& name) const {
 }
 
 size_t Document::SectionCount() const {
-    return 0;
+    return sections.size();
+}
+
+Document Load(istream& input) {
+    Document doc;
+    string line;
+    Section* section = nullptr;
+    while(getline(input, line)) {
+        if(!line.empty()) {
+            if(line[0] == '[') {
+                section = &doc.AddSection(line.substr(1, line.size() - 2));
+            } else {
+                auto [name, value] = Split(line, '=');
+                (*section)[string(name)] = string(value);
+            }
+        }
+    }
+    return doc;
 }
 
 }
--- C++/BrownBelt/2/spendings/ini/ini.h
+++ C++/BrownBelt/2/spendings/ini/ini.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <unordered_map>
+#include <string>
+#include <istream>
+
 using namespace std;
 
 namespace Ini {

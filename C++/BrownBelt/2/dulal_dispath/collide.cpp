1605329e8356dd2ff8411a44ad397dc336f38610
Виктор Краснов <vickr@inbox.ru>
2021-08-28 16:48:52 +0300

    * C++/BrownBelt/2/dual_dispatch (Двойная диспетчеризация (2d collisions)): solution

--- C++/BrownBelt/2/dulal_dispath/collide.cpp
+++ C++/BrownBelt/2/dulal_dispath/collide.cpp
@@ -8,144 +8,155 @@
 
 using namespace std;
 
-class Unit: public GameObject {
+template <typename T>
+struct Collider : GameObject {
+  bool Collide(const GameObject& that) const override {
+    return that.CollideWith(static_cast<const T&>(*this));
+  }
+};
+
+class Unit : public Collider<Unit> {
 public:
-  explicit Unit(geo2d::Point position): position_(position) {
+  Unit(geo2d::Point position) : position_(position) {
   }
 
   geo2d::Point GetPosition() const { return position_; }
 
-  bool Collide(const GameObject& that) const override {
-    return that.CollideWith(*this);
-  }
   bool CollideWith(const Unit& that) const override;
   bool CollideWith(const Building& that) const override;
   bool CollideWith(const Tower& that) const override;
   bool CollideWith(const Fence& that) const override;
 
 private:
-    geo2d::Point position_;
+  geo2d::Point position_;
 };
 
-class Building: public GameObject {
+class Building : public Collider<Building> {
 public:
-  explicit Building(geo2d::Rectangle geometry): position_(geometry) {
+  Building(geo2d::Rectangle geometry)
+    : geometry_(geometry)
+  {
   }
 
-  geo2d::Rectangle GetPosition() const { return position_; }
+  const geo2d::Rectangle& GetGeometry() const { return geometry_; }
 
-  bool Collide(const GameObject& that) const override {
-    return that.CollideWith(*this);
-  }
   bool CollideWith(const Unit& that) const override;
   bool CollideWith(const Building& that) const override;
   bool CollideWith(const Tower& that) const override;
   bool CollideWith(const Fence& that) const override;
 
 private:
-    geo2d::Rectangle position_;
+  geo2d::Rectangle geometry_;
 };
 
-class Tower: public GameObject {
+class Tower : public Collider<Tower> {
 public:
-  explicit Tower(geo2d::Circle geometry): position_(geometry) {
+  Tower(geo2d::Circle geometry)
+    : geometry_(geometry)
+  {
   }
 
-  geo2d::Circle GetPosition() const { return position_; }
+  const geo2d::Circle& GetGeometry() const { return geometry_; }
 
-  bool Collide(const GameObject& that) const override {
-    return that.CollideWith(*this);
-  }
   bool CollideWith(const Unit& that) const override;
   bool CollideWith(const Building& that) const override;
   bool CollideWith(const Tower& that) const override;
   bool CollideWith(const Fence& that) const override;
 
 private:
-    geo2d::Circle position_;
+  geo2d::Circle geometry_;
 };
 
-class Fence: public GameObject {
+class Fence : public Collider<Fence> {
 public:
-  explicit Fence(geo2d::Segment geometry): position_(geometry) {
+  Fence(geo2d::Segment geometry)
+    : geometry_(geometry)
+  {
   }
 
-  geo2d::Segment GetPosition() const { return position_; }
+  const geo2d::Segment& GetGeometry() const { return geometry_; }
 
-  bool Collide(const GameObject& that) const override {
-    return that.CollideWith(*this);
-  }
   bool CollideWith(const Unit& that) const override;
   bool CollideWith(const Building& that) const override;
   bool CollideWith(const Tower& that) const override;
   bool CollideWith(const Fence& that) const override;
 
 private:
-    geo2d::Segment position_;
+  geo2d::Segment geometry_;
 };
 
 // Unit CollideWith implementation
 
 bool Unit::CollideWith(const Unit& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
+  return geo2d::Collide(position_, that.position_);
 }
+
 bool Unit::CollideWith(const Building& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
+  return geo2d::Collide(position_, that.GetGeometry());
 }
+
 bool Unit::CollideWith(const Tower& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
+  return geo2d::Collide(position_, that.GetGeometry());
 }
+
 bool Unit::CollideWith(const Fence& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
+  return geo2d::Collide(position_, that.GetGeometry());
 }
+
+#define DEFINE_METHOD_COLLIDE_WITH(Class, ArgClass)       \
+  bool Class::CollideWith(const ArgClass& that) const {   \
+    return geo2d::Collide(geometry_, that.GetGeometry()); \
+  }
 
 // Building CollideWith implementation
 
 bool Building::CollideWith(const Unit& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
+  return geo2d::Collide(geometry_, that.GetPosition());
 }
-bool Building::CollideWith(const Building& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
-}
-bool Building::CollideWith(const Tower& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
-}
-bool Building::CollideWith(const Fence& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
-}
+
+DEFINE_METHOD_COLLIDE_WITH(Building, Building)
+DEFINE_METHOD_COLLIDE_WITH(Building, Tower)
+DEFINE_METHOD_COLLIDE_WITH(Building, Fence)
 
 // Tower CollideWith implementation
 
 bool Tower::CollideWith(const Unit& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
+  return geo2d::Collide(geometry_, that.GetPosition());
 }
-bool Tower::CollideWith(const Building& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
-}
-bool Tower::CollideWith(const Tower& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
-}
-bool Tower::CollideWith(const Fence& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
-}
+
+DEFINE_METHOD_COLLIDE_WITH(Tower, Building)
+DEFINE_METHOD_COLLIDE_WITH(Tower, Tower)
+DEFINE_METHOD_COLLIDE_WITH(Tower, Fence)
 
 // Fence CollideWith implementation
 
 bool Fence::CollideWith(const Unit& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
-}
-bool Fence::CollideWith(const Building& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
-}
-bool Fence::CollideWith(const Tower& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
-}
-bool Fence::CollideWith(const Fence& that) const {
-    return geo2d::Collide(GetPosition(), that.GetPosition());
+  return geo2d::Collide(geometry_, that.GetPosition());
 }
 
+DEFINE_METHOD_COLLIDE_WITH(Fence, Building)
+DEFINE_METHOD_COLLIDE_WITH(Fence, Tower)
+DEFINE_METHOD_COLLIDE_WITH(Fence, Fence)
+
 bool Collide(const GameObject& first, const GameObject& second) {
-    return first.Collide(second);
+  return first.Collide(second);
+  /* Альтернативное решение:
+  if (auto* unit = dynamic_cast<const Unit*>(&first)) {
+    if (auto* unit2 = dynamic_cast<const Unit*>(&second)) {
+      return geo2d::Collide(unit->GetPosition(), unit2->GetPosition());
+    } else if (auto* building = dynamic_cast<const Building*>(&second)) {
+      return geo2d::Collide(unit->GetPosition(), building->GetGeometry());
+    } else if (auto* tower = dynamic_cast<const Tower*>(&second)) {
+      return geo2d::Collide(unit->GetPosition(), tower->GetGeometry());
+    } else if (auto* fence = dynamic_cast<const Fence*>(&second)) {
+      return geo2d::Collide(unit->GetPosition(), fence->GetGeometry());
+    } else {
+      throw std::runtime_error("Failed to cast second to any known game object");
+    }
+  } else if (auto* building = dynamic_cast<const Building*>(&first)) {
+    ...
+  } ...
+  */
 }
 
 void TestAddingNewObjectOnMap() {
@@ -197,8 +208,96 @@ void TestAddingNewObjectOnMap() {
   ASSERT(!Collide(*new_defense_tower, *game_map[6]));
 }
 
+//////////////////////////////////////////////////////////////////////////
+/// Тесты ниже не являются частью решения. Они нужны для отладки!!!
+//////////////////////////////////////////////////////////////////////////
+
+void TestVectorProduct() {
+  using geo2d::Vector;
+  ASSERT_EQUAL((Vector{1, 0} * Vector{2, 0}), 0);
+  ASSERT_EQUAL((Vector{1, 0} * Vector{-1, 0}), 0);
+  ASSERT((Vector{1, 0} * Vector{1, 1} > 0));
+  ASSERT((Vector{1, 1} * Vector{1, 0} < 0));
+  ASSERT((Vector{1, 0} * Vector{-1, 1} > 0));
+}
+
+void TestPointSegmentCollide() {
+  using geo2d::Point;
+  using geo2d::Segment;
+
+  ASSERT(geo2d::Collide(Point{1, 0}, Segment{{0, 0}, {10, 0}}));
+  ASSERT(geo2d::Collide(Point{0, 0}, Segment{{0, 0}, {10, 0}}));
+  ASSERT(geo2d::Collide(Point{10, 0}, Segment{{0, 0}, {10, 0}}));
+  ASSERT(geo2d::Collide(Point{3, 3}, Segment{{0, 0}, {10, 10}}));
+
+  ASSERT(!geo2d::Collide(Point{-1, 0}, Segment{{0, 0}, {10, 0}}));
+  ASSERT(!geo2d::Collide(Point{11, 0}, Segment{{0, 0}, {10, 0}}));
+  ASSERT(!geo2d::Collide(Point{1, 1}, Segment{{0, 0}, {10, 0}}));
+  ASSERT(!geo2d::Collide(Point{5, 5}, Segment{{0, 0}, {10, 0}}));
+  ASSERT(!geo2d::Collide(Point{-5, 0}, Segment{{0, 0}, {10, 0}}));
+  ASSERT(!geo2d::Collide(Point{5, -1}, Segment{{0, 0}, {10, 0}}));
+}
+
+void TestPointRectangleCollide() {
+  using geo2d::Point;
+  using geo2d::Rectangle;
+
+  const Rectangle r{{0, 0}, {5, 3}};
+  ASSERT(geo2d::Collide(Point{0, 0}, r));
+  ASSERT(geo2d::Collide(Point{5, 0}, r));
+  ASSERT(geo2d::Collide(Point{5, 3}, r));
+  ASSERT(geo2d::Collide(Point{0, 3}, r));
+  ASSERT(geo2d::Collide(Point{2, 2}, r));
+  ASSERT(geo2d::Collide(Point{1, 3}, r));
+
+  ASSERT(!geo2d::Collide(Point{-1, 0}, r));
+  ASSERT(!geo2d::Collide(Point{0, -1}, r));
+  ASSERT(!geo2d::Collide(Point{0, 4}, r));
+  ASSERT(!geo2d::Collide(Point{6, 0}, r));
+  ASSERT(!geo2d::Collide(Point{5, 4}, r));
+  ASSERT(!geo2d::Collide(Point{6, 3}, r));
+  ASSERT(!geo2d::Collide(Point{2, 8}, r));
+}
+
+void TestSegmentSegmentCollide() {
+  using geo2d::Segment;
+  ASSERT(geo2d::Collide(Segment{{0, 0}, {2, 2}}, Segment{{2, 0}, {0, 2}}));
+  ASSERT(geo2d::Collide(Segment{{0, 0}, {2, 2}}, Segment{{2, 0}, {1, 1}}));
+  ASSERT(geo2d::Collide(Segment{{0, 0}, {10, 6}}, Segment{{5, 3}, {15, 9}}));
+  ASSERT(geo2d::Collide(Segment{{0, 0}, {6, 2}}, Segment{{4, 2}, {6, 0}}));
+  ASSERT(geo2d::Collide(Segment{{0, 0}, {6, 2}}, Segment{{6, 2}, {6, 3}}));
+
+  ASSERT(!geo2d::Collide(Segment{{0, 0}, {2, 2}}, Segment{{2, 0}, {1, 0}}));
+  ASSERT(!geo2d::Collide(Segment{{0, 0}, {10, 6}}, Segment{{5, 4}, {15, 10}}));
+  ASSERT(!geo2d::Collide(Segment{{0, 0}, {6, 2}}, Segment{{4, 1}, {6, 0}}));
+}
+
+void TestSegmentCircleCollide() {
+  using geo2d::Circle;
+  using geo2d::Segment;
+  const Circle c{{0, 0}, 4};
+
+  ASSERT(geo2d::Collide(c, Segment{{0, 0}, {1, 0}}));
+  ASSERT(geo2d::Collide(c, Segment{{3, 1}, {10, 1}}));
+  ASSERT(geo2d::Collide(c, Segment{{-5, 2}, {5, 2}}));
+  ASSERT(geo2d::Collide(c, Segment{{-5, 3}, {5, 3}}));
+  ASSERT(geo2d::Collide(c, Segment{{-5, 4}, {5, 4}}));
+  ASSERT(geo2d::Collide(c, Segment{{3, 1}, {4, 5}}));
+  ASSERT(geo2d::Collide(c, Segment{{5, 0}, {-2, 4}}));
+
+  ASSERT(!geo2d::Collide(c, Segment{{4, 1}, {4, 5}}));
+  ASSERT(!geo2d::Collide(c, Segment{{-5, 5}, {5, 5}}));
+  ASSERT(!geo2d::Collide(c, Segment{{4, 4}, {5, 4}}));
+  ASSERT(!geo2d::Collide(Circle{{10, 7}, 1}, Segment{{7, 3}, {9, 8}}));
+}
+
 int main() {
   TestRunner tr;
   RUN_TEST(tr, TestAddingNewObjectOnMap);
+  RUN_TEST(tr, TestVectorProduct);
+  RUN_TEST(tr, TestPointSegmentCollide);
+  RUN_TEST(tr, TestPointRectangleCollide);
+  RUN_TEST(tr, TestSegmentSegmentCollide);
+  RUN_TEST(tr, TestSegmentCircleCollide);
   return 0;
 }

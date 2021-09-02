e463c5de2755dae691dde158109f77bed74bb97f
Виктор Краснов <vickr@inbox.ru>
2021-09-02 12:43:15 +0300

    + C++/BrownBelt/3/object_pool (Пул объектов): solution (решал в Красном поясе (правда, не смог найти того решения - как будто не запулил))

--- C++/BrownBelt/3/object_pool/object_pool.cpp
+++ C++/BrownBelt/3/object_pool/object_pool.cpp
@@ -1,7 +1,6 @@
 #include "test_runner.h"
 
 #include <algorithm>
-#include <iostream>
 #include <string>
 #include <queue>
 #include <stdexcept>
@@ -19,33 +18,46 @@ public:
   ~ObjectPool();
 
 private:
-  // Добавьте сюда поля
+  queue<T*> free;
+  set<T*> allocated;
 };
 
-void TestObjectPool() {
-  ObjectPool<string> pool;
-
-  auto p1 = pool.Allocate();
-  auto p2 = pool.Allocate();
-  auto p3 = pool.Allocate();
-
-  *p1 = "first";
-  *p2 = "second";
-  *p3 = "third";
-
-  pool.Deallocate(p2);
-  ASSERT_EQUAL(*pool.Allocate(), "second");
-
-  pool.Deallocate(p3);
-  pool.Deallocate(p1);
-  ASSERT_EQUAL(*pool.Allocate(), "third");
-  ASSERT_EQUAL(*pool.Allocate(), "first");
-
-  pool.Deallocate(p1);
+template <typename T>
+T* ObjectPool<T>::Allocate() {
+  if (free.empty()) {
+    free.push(new T);
+  }
+  auto ret = free.front();
+  free.pop();
+  allocated.insert(ret);
+  return ret;
 }
 
-int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestObjectPool);
-  return 0;
+template <typename T>
+T* ObjectPool<T>::TryAllocate() {
+  if (free.empty()) {
+    return nullptr;
+  }
+  return Allocate();
+}
+
+template <typename T>
+void ObjectPool<T>::Deallocate(T* object) {
+  if (allocated.find(object) == allocated.end()) {
+    throw invalid_argument("");
+  }
+  allocated.erase(object);
+  free.push(object);
+}
+
+template <typename T>
+ObjectPool<T>::~ObjectPool() {
+  for (auto x : allocated) {
+    delete x;
+  }
+  while (!free.empty()) {
+    auto x = free.front();
+    free.pop();
+    delete x;
+  }
 }

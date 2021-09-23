925629a274f591da94fe82c37b17cb12201d26ae
Victor <sirvickr@gmail.com>
2021-09-23 13:03:02 +0300

    * C++/BrownBelt/4/cache (Cache): done

--- C++/BrownBelt/4/cache/Solution.cpp
+++ C++/BrownBelt/4/cache/Solution.cpp
@@ -1,19 +1,84 @@
 #include "Common.h"
 
+#include <list>
+#include <unordered_map>
+#include <mutex>
+
 using namespace std;
 
 class LruCache : public ICache {
+  struct RankItem;
+  struct StorageItem;
+
+  using Rank = list<RankItem>;
+
+  struct StorageItem {
+    BookPtr book_ptr;
+    Rank::iterator rank_it;
+  };
+  
+  using Storage = unordered_map<string, StorageItem>;
+  
+  struct RankItem {
+    size_t size;
+    Storage::iterator storage_it;
+  };
+
 public:
   LruCache(
       shared_ptr<IBooksUnpacker> books_unpacker,
       const Settings& settings
-  ) {
-    // реализуйте метод
-  }
+  ): books_unpacker_(move(books_unpacker)), settings_(settings) {}
 
   BookPtr GetBook(const string& book_name) override {
-    // реализуйте метод
+    BookPtr book;
+    lock_guard lg(m_);
+    auto it = storage_.find(book_name);
+    if(it == storage_.end()) {
+    
+      book = books_unpacker_->UnpackBook(book_name);
+      size_t size = book->GetContent().size();
+
+      bool too_big = false;
+      while(total_size_ + size > settings_.max_memory) {
+        if(rank_.empty()) {
+          too_big = true;
+          break;
+        }
+        const RankItem& item = rank_.back();
+        total_size_ -= item.size;
+        storage_.erase(item.storage_it);
+        rank_.pop_back();
+      }
+
+      if(!too_big) {
+        it = storage_.insert({book_name, {book, {}}}).first;
+        rank_.push_front({size, it});
+        it->second.rank_it = rank_.begin();
+        total_size_ += size;
+      }
+    
+    } else {
+    
+      book = it->second.book_ptr;
+      size_t size = book->GetContent().size();
+      
+      rank_.erase(it->second.rank_it);
+      rank_.push_front({size, it});
+      it->second.rank_it = rank_.begin();
+    
+    }
+    
+    return book;
   }
+
+private:
+  mutex m_;
+  shared_ptr<IBooksUnpacker> books_unpacker_;
+  Settings settings_;
+  Storage storage_;
+  Rank rank_;
+  size_t total_size_ = 0;
 };
 
 
@@ -21,5 +86,5 @@ unique_ptr<ICache> MakeCache(
     shared_ptr<IBooksUnpacker> books_unpacker,
     const ICache::Settings& settings
 ) {
-  // реализуйте функцию
+  return make_unique<LruCache>(move(books_unpacker), settings);
 }

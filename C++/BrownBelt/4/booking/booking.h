d02b7940c58b3bb77b218428a6456afdddda6b3b
Victor <sirvickr@gmail.com>
2021-09-23 18:26:48 +0300

    * C++/BrownBelt/4/booking (RAII Booking): solution

--- C++/BrownBelt/4/booking/booking.h
+++ C++/BrownBelt/4/booking/booking.h
@@ -1,21 +1,28 @@
 #pragma once
 
+#include <utility>
+
 namespace RAII {
 
   template<class Provider>
   class Booking {
   private:
+    using BookingId = typename Provider::BookingId;
+    
     Provider* provider_;
+    BookingId id_;
 
   public:
-    Booking(Provider* provider, int)
-      : provider_(provider) 
+    Booking(Provider* provider, const BookingId& id)
+      : provider_(provider)
+      , id_(id)
     {}
 
     Booking(const Booking&) = delete;
 
     Booking(Booking&& other)
       : provider_(other.provider_) 
+      , id_(other.id_)
     {
       other.provider_ = nullptr;
     }
@@ -23,9 +30,14 @@ namespace RAII {
     Booking& operator=(const Booking&) = delete;
 
     Booking& operator=(Booking&& other) {
-      provider_ = other.provider_;
-      other.provider_ = nullptr;
+      std::swap(provider_, other.provider_);
+      std::swap(id_, other.id_);
       return *this;
+    }
+
+    // Эта функция не требуется в тестах, но в реальной программе она может быть нужна
+    BookingId GetId() const {
+      return id_;
     }
 
     ~Booking() {
